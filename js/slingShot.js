e9ab4c2218a4ca01a57cc266e635c64546126f61
neutronpop <85616106+neutronpop@users.noreply.github.com>
2021-06-27 11:09:05 +0530

    Add files via upload

---
+++ js/slingShot.js
@@ -0,0 +1,33 @@
+class slingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 20
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(stoneObj){
+
+    this.sling.bodyA = stoneObj;    
+
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
